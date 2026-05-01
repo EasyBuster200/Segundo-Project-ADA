b938452ff3569c13db467b44126600a517d10817
duarte-Win <duartecruzcoelho@gmail.com>
2026-05-01 20:20:25 +0100

    changes

--- src/MagicBeams.java
+++ src/MagicBeams.java
@@ -32,58 +32,74 @@ public class MagicBeams {
     }
   }
 
-  public List<Integer> solve() throws Exception{
+  public List<Integer> solve() throws Exception {
     List<Integer> answer = new LinkedList<>();
     Set<Beam> chosenBeams = new HashSet<>();
     Queue<Beam> notProcessed = new LinkedList<>();
+    Map<Integer, Integer> degrees = new HashMap<>();
+    Map<Integer, Set<Beam>> adj = new HashMap<>();
 
-    for (int c = L; c < N+L; c++) { // get chosen beams
-        for (int r = 0; r < R; r++) {
-            Beam beam = grid[r][c];
+    for (int c = L; c < N + L; c++) { // get beams in the collumns to be cleared
+      for (int r = 0; r < R; r++) {
+        Beam beam = grid[r][c];
 
-            if (beam != null) {
-                chosenBeams.add(beam);
-                notProcessed.add(beam);
-            }
+        if (beam != null) {
+          chosenBeams.add(beam);
+          notProcessed.add(beam);
         }
+      }
     }
 
-    if(chosenBeams.isEmpty())
-        throw new Exception("False Alarm");
+    for (Beam beam : chosenBeams) {
+      degrees.putIfAbsent(beam.id(), 0);
+      adj.putIfAbsent(beam.id(), new HashSet<>());
+    }
+
+    if (chosenBeams.isEmpty())
+      throw new Exception("False Alarm");
 
     Set<Beam> processed = new HashSet<>();
-    while (!notProcessed.isEmpty()) {
-        Beam beam = notProcessed.poll();
+    while (!notProcessed.isEmpty()) { // Get beams that block the already choosen beams
+      Beam beam = notProcessed.poll();
 
-        if (processed.contains(beam))
-            continue;
+      if (processed.contains(beam))
+        continue;
 
-        processed.add(beam);
+      processed.add(beam); // Don't need to check if contains, because add already checks it. (Sets can't
+                           // have duplicates
 
-        int tailR = beam.r() + (beam.length() - 1) * beam.direction()[0];
-        int blockerR = tailR + beam.direction()[0];
-        
-        int tailC = beam.c() + (beam.length() - 1) * beam.direction()[1];
-        int blockerC = tailC + beam.direction()[1];
+      int tailR = beam.r() + (beam.length() - 1) * beam.direction()[0];
+      int blockerR = tailR + beam.direction()[0];
 
-        while (blockerR >= 0 && blockerR < R && blockerC >= 0 && blockerC < C) {
-            Beam blocker = grid[blockerR][blockerC];
+      int tailC = beam.c() + (beam.length() - 1) * beam.direction()[1];
+      int blockerC = tailC + beam.direction()[1];
 
-            if(blocker != null && blocker != beam){ //Do we need this != beam
-                notProcessed.add(blocker);
-                chosenBeams.add(blocker);
-            }
-            // notProcesed
-            // chosenBeams = 1,3,5,6,4
-            blockerR += beam.direction()[0];
-            blockerC += beam.direction()[1];
+      while (blockerR >= 0 && blockerR < R && blockerC >= 0 && blockerC < C) {
+        Beam blocker = grid[blockerR][blockerC];
+
+        if (blocker != null && blocker != beam) { // Do we need this != beam
+          notProcessed.add(blocker);
+          chosenBeams.add(blocker);
+
+          degrees.putIfAbsent(blocker.id(), 0);
+          adj.putIfAbsent(blocker.id(), new HashSet<>());
+
+          degrees.put(beam.id(), degrees.get(beam.id()) + 1);
+          adj.get(blocker.id()).add(beam);
         }
-        // Set<Intenger>  dontHaveBlocker 3,5,
+
+        blockerR += beam.direction()[0];
+        blockerC += beam.direction()[1];
+      }
+
     }
+
+    //Loop choosen
+    //
 
     return answer;
 
-    }
+  }
 
   static int[] convertDirection(char dir) {
     switch (dir) {
@@ -99,4 +115,26 @@ public class MagicBeams {
         return new int[] { 0, 0 };
     }
   }
+
+  private int getMinKey() {
+    int min = 0;
+    for(Integer node: degrees) {
+      if(degrees.get(node) == 0 && node < min)
+        min = node;
+    }
+
+    return min;
+  }
 }
+
+// My idea, have a way of knowing what beam blocks each beam, for our example
+// know that beam 1 is blocked by beam 4
+// then go through all the beams in order from the example 1 -> 6
+// always grab the lowest with no blockers and remove in our case 3 is the
+// lowest and has no blockers
+// next step 4 is the lowest and has no blockers
+// 1 is the lowest and has no blokcers
+// 5 is the lowest and has no blockers
+// 6 is the lowest and has no blockers
+
+// result [3,4,1,5,6]
