1a6e0dc863573fd4e68c16384173220ee3871650
duarte-Win <duartecruzcoelho@gmail.com>
2026-05-08 17:06:29 +0100

    Added all the needed comments

--- src/MagicBeams.java
+++ src/MagicBeams.java
@@ -19,7 +19,8 @@ public class MagicBeams {
   private Queue<Beam> notProcessed = new ArrayDeque<>();
   private Set<Integer> inQueue = new HashSet<>();
   private Map<Integer, Integer> degrees = new HashMap<>();
-  private Map<Integer, Set<Integer>> adj = new HashMap<>(); // Blocker -> Blocked
+  // Key: id Beam -> List of Beam ids that the key blocks
+  private Map<Integer, Set<Integer>> adj = new HashMap<>();
 
   public MagicBeams(int R, int C, int N, int L, int B) {
     this.R = R;
@@ -30,6 +31,15 @@ public class MagicBeams {
     beams = new Beam[B + 1];
   }
 
+  /**
+   * Adds a new Beam to the program
+   * 
+   * @param id : unique id of the Beam
+   * @param r  : starting line of the Beam
+   * @param c  : starting collumn of the Beam
+   * @param l  : length of the Beam
+   * @param d  : character representing the Beams direction (E, W, S, N)
+   */
   public void addBeam(int id, int r, int c, int l, char d) {
     int[] direction = convertDirection(d);
     Beam beam = new Beam(r, c, l, id, direction);
@@ -38,6 +48,8 @@ public class MagicBeams {
     for (int i = 0; i < l; i++) {
       grid[r][c] = id;
 
+      // When adding a new beam, we check if its in one of the collumns to be cleared,
+      // and if it is we add it to the queue of beams to be processed
       if (L <= c && c < N + L) {
         if (inQueue.add(beam.id()))
           notProcessed.add(beam);
@@ -51,16 +63,23 @@ public class MagicBeams {
     }
   }
 
+  /**
+   * Calculates in what order beams need to be removed to clear certain collumns
+   * of Beams
+   * 
+   * @return list with the ids of the beams to be removed, in the order they
+   *         should be removed
+   */
   public List<Integer> solve() throws Exception {
     List<Integer> answer = new ArrayList<>();
 
-    // If there are no beams to remove then its a "False alarm"
+    // First check if there are Beams to remove
     if (inQueue.isEmpty())
       throw new Exception("False alarm");
 
     /*
-     * Next we need to find any beams that block selected beams
-     */;
+     * Next we need to find any beams that block the ones to be removed
+     */
     while (!notProcessed.isEmpty()) {
       Beam beam = notProcessed.poll();
 
@@ -98,12 +117,22 @@ public class MagicBeams {
 
     }
 
+    // List with all the Beams that have no blockers (degree == 0)
     PriorityQueue<Integer> zeroDegree = new PriorityQueue<>();
 
     for (Map.Entry<Integer, Integer> e : degrees.entrySet()) {
       if (e.getValue() == 0)
         zeroDegree.add(e.getKey());
     }
+
+    /*
+     * Filling the answer list
+     * 
+     * As long as we have beams to be removed with degree == 0 we:
+     * - Pick the lowest id beam with degree 0, and add it to the answer
+     * - Lower the degree of any beams it blocks
+     * - If the lowered degrees become 0, they get added to the list of zeroDegree
+     */
 
     while (!zeroDegree.isEmpty()) {
       int id = zeroDegree.poll();
@@ -121,6 +150,8 @@ public class MagicBeams {
       degrees.remove(id);
     }
 
+    // If we tried to remove all the beams that needed to be removed, and there asre
+    // still beams left, then there is no way to remove them without a disaster
     if (!degrees.isEmpty()) {
       throw new Exception("Disaster");
     }
@@ -129,6 +160,13 @@ public class MagicBeams {
 
   }
 
+  /**
+   * Helper method that converts the direction char into an array of integers
+   * representing the direction
+   * 
+   * @param dir : Character representing the direction of a Beam
+   * @return a numerical representation of that direction
+   */
   static int[] convertDirection(char dir) {
     switch (dir) {
       case 'N':
